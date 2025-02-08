a7098e2392c9657788fd02788eccb9becfcf493a
justparthi <justparthiban@gmail.com>
2025-02-08 19:26:23 +0530

    Read Data from db

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Authenticate from './Components/Authenticate.jsx'
+import { db } from './Config/firebase.js';
+import { getDocs, collection } from 'firebase/firestore';
 
 
 const App = () => {
+
+  const [ movieList, setMovieList ] = useState([]);
+
+  const movieCollectionRef = collection(db, "Movies")
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        const data = await getDocs(movieCollectionRef);
+ 
+        const filterData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(filterData);
+      } catch (err) {
+        console.error(err)
+        
+      }
+
+    }
+    getMovieList()
+  }, [])
+
+
 
   console.log("App component rendered"); 
   return (
